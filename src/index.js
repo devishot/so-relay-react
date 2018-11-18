621f77d6815f79fe3537a6d44575a606cebd2068
Sattar Stamkulov <sattar.coder@gmail.com>
2018-11-19 01:36:33 +0600

    delete: SingleTimeRecord test query

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import { App } from './modules/app';
-import { SingleTimeRecord } from './modules/time_record';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App>
-    <SingleTimeRecord id="B58CC80A-1BF5-4652-9559-97AC6C6545AD" />
   </App>
 , document.getElementById('root'));
 registerServiceWorker();
