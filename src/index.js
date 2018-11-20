d91dd8a2aa6ac1ca84ff45b000ce2808e7f6eb91
Sattar Stamkulov <sattar.coder@gmail.com>
2018-11-20 20:58:08 +0600

    added: ClientPage component

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 import { App } from './modules/app';
+import { ClientPage } from './modules/client/queries/ClientPage';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App>
+    <ClientPage />
   </App>
 , document.getElementById('root'));
 registerServiceWorker();
---
+++ src/modules/client/queries/ClientPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { QueryRenderer } from 'react-relay';
+
+import environment from '../../../base/Environment';
+import { 
+    ClientProjectList, 
+    clientProjectListSpec 
+} from '../../client_project/queries/ClientProjectList';
+
+
+class ClientPage extends React.Component {
+    render() {
+        return (
+            <QueryRenderer
+                environment={environment}
+                variables={{
+                    clientID: '00c3c715-9c9f-49be-b336-6d661f2bf561',
+                    count: 20,
+                }}
+                query={clientProjectListSpec}
+                render={({error, props}) => {
+                    if (error) {
+                        return <div>{error.message}</div>
+                    } else if (props) {
+                        return <ClientProjectList client={props.client} />
+                    }
+                    return <div>Loading</div>
+                }}
+            />
+        )
+    }
+}
+
+export { ClientPage };
